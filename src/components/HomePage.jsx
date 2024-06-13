3e25c8f29f74bcbf35189a3ea6fb9c7e1c59cbc0
shafnashakeer <shafnasm20@gmail.com>
2024-06-13 23:15:25 +0530

    NavPage created

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavPage from './NavPage'
 
 const HomePage = () => {
     return (
         <div>
+            <NavPage/>
             <div className="container">
                 <div className="row g-3">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 xxl-12">
---
+++ src/components/NavPage.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavPage = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <Link class="navbar-brand" to="#">MY KSRTC</Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link" to="/login">LogIn</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link" to="/addbus">Add Bus</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link " to="/search">Search</Link>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/viewall">View All</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavPage
