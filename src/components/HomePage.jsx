2621263760d9802272f37ff13c16047d1b0a16d9
shafnashakeer <shafnasm20@gmail.com>
2024-06-13 22:30:54 +0530

    Home Page completed

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,87 @@
 import React from 'react'
 
 const HomePage = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div>
+            <div className="container">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 xxl-12">
+                        <div class="card" >
+                            <img src="https://play-lh.googleusercontent.com/MQG2NtQ3faNmFHfPZIg2FNSeIp4PAY6-7Z87xJxcqDwtldJ6X4vNcQ826FWmNeq5BA=w526-h296-rw" height="300px" class="card-img-top" alt="..."></img>
+                        </div>
+                        <div className="row g-3">
+                            <div className=" col col-12 col-4 col sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="https://previews.123rf.com/images/martialred/martialred1903/martialred190300015/120162077-black-and-white-handshake-or-shaking-hands-in-unity-flat-vector-icon-for-apps-and-websites.jpg"
+                                        height="250px" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">INTRODUCTION</h5>
+
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=" col col-12 col-4 col sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="https://img.freepik.com/premium-vector/services-concept-idea-customer-support-help-clients-with-problems-assistance-providing-customer-with-valuable-information-illustration_277904-4759.jpg"
+                                        height="250px" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">OUR SEVICES </h5>
+
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=" col col-12 col-4 col sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="https://img.freepik.com/free-vector/hand-drawn-credit-assessment-concept-with-documents_23-2149154259.jpg"
+                                        height="250px" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">DOCUMENT</h5>
+
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=" col col-12 col-4 col sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="https://img.freepik.com/premium-vector/store-merchandiser-online-service-platform_277904-8611.jpg"
+                                        height="250px" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">SEVICES AND SCHEMES</h5>
+
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=" col col-12 col-4 col sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="https://www.shutterstock.com/shutterstock/photos/606092714/display_1500/stock-vector-phone-number-vector-icon-flat-cobalt-symbol-pictogram-is-isolated-on-a-white-background-designed-606092714.jpg"
+                                        height="250px" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">ENQUERY</h5>
+
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=" col col-12 col-4 col sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="https://cdn1.vectorstock.com/i/1000x1000/35/75/enquiry-rubber-stamp-vector-12943575.jpg"
+                                        height="250px" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">CONTACT</h5>
+
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default HomePage
